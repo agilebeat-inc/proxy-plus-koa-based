2222e1ba4d7689916897bb6af6e8f6948d89c300
Marek Dwulit <marek.dwulit@gmail.com>
2025-07-16 19:58:00 +0000

    Fix build process

--- src/middleware/pep.ts
+++ src/middleware/pep.ts
@@ -4,12 +4,12 @@ import { Middleware } from 'koa';
 
 export const pepMiddleware: Middleware = async (ctx, next) => {
 
-  if (token) {
-    try {
-      };
-    } catch (err) {
-      console.warn('Invalid token');
-    }
-  }
+  // if (token) {
+  //   try {
+  //     };
+  //   } catch (err) {
+  //     console.warn('Invalid token');
+  //   }
+  // }
   await next();
 };
