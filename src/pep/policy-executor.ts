1ef846e21da6c83f128baa1d0317374f95f0c54f
Marek Dwulit <marek.dwulit@gmail.com>
2025-08-26 21:53:59 +0000

    PEP verion allow to extend policy taking the resource (path) and user name, look it up against external service and render decision yeay or nay in terms of allowing user pass through

--- src/pep/policy-executor.ts
+++ src/pep/policy-executor.ts
@@ -1,7 +1,7 @@
 import { loadPolicy } from './policy-loader';
 import { getEnvVar } from '../utils/envHelper';
 
-const policyName = getEnvVar('PEP-POLICY-NAME', 'simple-role-admin');
+const policyName = getEnvVar('PEP-POLICY-NAME', 'mock-always-deny');
 
 function getPolicyName() {
   return policyName;
